32d85ed76a017c66d2a3bb6f71349dda93c1b6a1
Kevin Le Perf <codlabtech@gmail.com>
2023-10-01 15:57:17 +0200

    Fix jvm

--- lorcana-server/downloader/build.gradle.kts
+++ lorcana-server/downloader/build.gradle.kts
@@ -27,8 +27,8 @@ application {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
